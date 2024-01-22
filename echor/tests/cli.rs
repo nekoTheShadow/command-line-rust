dc8939d0b6914d08fd29fae502bc6d7c187c4f3f
nekoTheShadow <h.nakamura0903@gmail.com>
2024-01-22 14:42:40 +0900

    2.3.2 プログラムの出力を比較する

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,7 +1,17 @@
+use std::fs;
+
 use assert_cmd::Command;
 
 #[test]
 fn dies_no_args() {
     let mut cmd = Command::cargo_bin("echor").unwrap();
     cmd.arg("hello").assert().success();
+}
+
+#[test]
+fn hello1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut cmd = Command::cargo_bin("echor").unwrap();
+    cmd.arg("Hello there").assert().success().stdout(expected);
 }
