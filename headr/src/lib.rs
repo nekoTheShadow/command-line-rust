9932d5f6658d7dfe62ea3e68f545ef886d8ce0b8
nekoTheShadow <h.nakamura0903@gmail.com>
2024-01-23 23:40:05 +0900

    4.2.3 引数の定義

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use clap::App;
+use clap::{App, Arg};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -21,8 +21,18 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Hajime Nakamura <h.nakamura0903@gmail.com>")
         .about("Rust head")
+        .arg(Arg::with_name("lines").short("n").long("lines").value_name("LINES").help("Number of lines").default_value("10"))
+        .arg(Arg::with_name("bytes").short("c").long("bytes").value_name("BYTES").takes_value(true).conflicts_with("lines").help("Number of lines"))
+        .arg(Arg::with_name("files").value_delimiter("FILE").help("Input files(s)").multiple(true).default_value("-"))
         .get_matches();
-    unimplemented!()
+
+    let lines = matches.value_of("lines").map(parse_positive_int).transpose().map_err(|e| format!("illegal line count -- {}", e))?;
+    let bytes = matches.value_of("bytes").map(parse_positive_int).transpose().map_err(|e| format!("illegal byte count -- {}", e))?;
+    Ok(Config{
+        files : matches.values_of_lossy("files").unwrap(),
+        lines : lines.unwrap(),
+        bytes
+    })
 }
 
 fn parse_positive_int(val: &str) -> MyResult<usize> {
