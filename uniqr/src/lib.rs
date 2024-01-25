242e4bc9314187e39a88f84e17b7e11118331f0f
nekoTheShadow <h.nakamura0903@gmail.com>
2024-01-25 19:25:18 +0900

    6.3 解答例

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fmt::format, fs::File, io::{self, BufRead, BufReader}};
+use std::{error::Error, fs::File, io::{self, BufRead, BufReader, Write}};
 
 use clap::{App, Arg};
 
@@ -12,16 +12,45 @@ pub struct Config {
 }
 
 pub fn run(config: Config) -> MyResult<()> {
+    let mut out_file: Box<dyn Write>  = match &config.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+    let mut print = |count: u64, text: &str| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
+
+
     let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
     let mut line = String::new();
+    let mut previous = String::new();
+    let mut count = 0;
+
     loop {
         let bytes = file.read_line(&mut line)?;
         if bytes == 0 {
             break;
         }
-        print!("{}", line);
+
+        if line.trim_end() != previous.trim_end() {
+            print(count, &previous)?;
+            previous = line.clone();
+            count = 0;
+        }
+
+        count += 1;
         line.clear();
     }
+
+    print(count, &previous)?;
+
     Ok(())
 }
 
