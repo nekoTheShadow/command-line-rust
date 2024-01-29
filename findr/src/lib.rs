35e58f07dd2c297b375480ccc278c2998d1d144e
nekoTheShadow <h.nakamura0903@gmail.com>
2024-01-29 22:25:12 +0900

    7.2.2 引数の検証

--- findr/src/lib.rs
+++ findr/src/lib.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 
-use clap::App;
+use clap::{App, Arg};
 use regex::Regex;
+
+use crate::EntryType::*;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -24,8 +26,31 @@ pub fn get_args() -> MyResult<Config> {
         .version("0.1.0")
         .author("Hajime Nakamura <h.nakamura0903@gmail.com>")
         .about("Rust find")
+        .arg(Arg::with_name("paths").value_name("PATH").help("Search paths").default_value(".").multiple(true))
+        .arg(Arg::with_name("names").value_name("NAME").short("n").long("name").help("Name").takes_value(true).multiple(true))
+        .arg(Arg::with_name("types").value_name("TYPE").short("t").long("type").help("Entry type").possible_values(&["f", "d", "r"]).takes_value(true).multiple(true))
         .get_matches();
-    unimplemented!();
+
+    let names  = matches.values_of_lossy("names").map(|vals| {
+        vals.into_iter()
+            .map(|name| Regex::new(&name).map_err(|_| format!("Invalid --name \"{}\"", name)))
+            .collect::<Result<Vec<_>, _>>()
+    }).transpose()?.unwrap_or_default();
+    let entry_types = matches.values_of_lossy("types").map(|vals| {
+        vals.iter().map(|val| match val.as_str() {
+            "d" => Dir,
+            "f" => File,
+            "l" => Link,
+            _ => unreachable!("Invalid type"),
+        }).collect()
+    }).unwrap_or_default();
+
+
+    Ok(Config {
+        paths: matches.values_of_lossy("paths").unwrap(),
+        names,
+        entry_types,
+    })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
