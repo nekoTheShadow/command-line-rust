2d0f254b7383297cecb1fa01491a14d5e6010500
nekoTheShadow <h.nakamura0903@gmail.com>
2024-02-11 18:22:29 +0900

    11.2.2 政府の数値引数の解析

--- tailr/src/lib.rs
+++ tailr/src/lib.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 
 use clap::App;
 
+
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug, PartialEq)]
@@ -30,4 +31,62 @@ pub fn get_args() -> MyResult<Config> {
 pub fn run(config: Config) -> MyResult<()> {
     println!("{:#?}", config);
     Ok(())
+}
+
+fn parse_num(val: &str) -> MyResult<TakeValue> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_num;
+    use crate::TakeValue::*;
+
+    
+    #[test]
+    fn test_parse_num() {
+        let res = parse_num("3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(-3));
+
+        let res = parse_num("+3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(3));
+
+        let res = parse_num("-3");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(-3));
+
+        let res = parse_num("0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(0));
+
+        let res = parse_num("+0");
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), PlusZero);
+
+        let res = parse_num(&i64::MAX.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN + 1));
+
+        let res = parse_num(&(i64::MIN + 1).to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN + 1));
+
+        let res = parse_num(&format!("+{}", i64::MAX));
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MAX));
+
+        let res = parse_num(&i64::MIN.to_string());
+        assert!(res.is_ok());
+        assert_eq!(res.unwrap(), TakeNum(i64::MIN));
+
+        let res = parse_num("3.14");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "3.14");
+
+        let res = parse_num("foo");
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "foo");
+    }
 }
